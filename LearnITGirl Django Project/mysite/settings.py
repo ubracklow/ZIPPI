7093e27a1c9d1690a32a66e2dedefb7d73e79c86
ubracklow <ubracklow@hotmail.com>
2015-01-25 18:43:27 +0100

    db settings changed, css and templates

--- LearnITGirl Django Project/mysite/settings.py
+++ LearnITGirl Django Project/mysite/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zippi',
+        'USER': 'zippi',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
